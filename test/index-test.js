5f02c892fc9262fc6cb600b787487cd8887840f8
Corey <coreyzhee@gmail.com>
2022-09-07 17:08:50 -0400

    commit tree baaa7f6797714d961c8a4760d7aed0b795b1c021

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,6 @@
-
+function logTwoValues(value1, value2) {
+  console.log(`The two values are ${value1} and ${value2}.`);
+}
 describe('introduction(name)', function() {
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
     expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
